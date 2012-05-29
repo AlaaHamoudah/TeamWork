de15dd81dd7443572f49b08c0328a52ce3712dd8
Alaa Hamoudah <alaakhalil11@gmail.com>
2012-05-29 05:32:57 +0300

    Create main index in view folder

---
+++ view/index.php
@@ -0,0 +1,15 @@
+
+<html>
+    <head>hello world!</head>
+    <body>
+        <h1>test</h1>
+    </body>
+</html>
+
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
